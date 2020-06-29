b41ceba107709a83de112edce25fcc4ec21ddc6b
Ellie Wetzel <elliehw2005@gmail.com>
2020-06-28 17:26:51 -0700

    Switch statements and horoscope(ip)

--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -12,6 +12,31 @@ public class CustomButtonOptionPanes {
 		String choice = options[input];
 		
 		//use a switch statement to do something cool for each option
+		switch (choice) {
+		case "Sunday":
+			System.out.println("Yay! You have the day off to relax.");
+			break;
+		case "Monday":
+			System.out.println("First day of the week, how does it feel?");
+			break;
+		case "Tuesday":
+			System.out.println("How's it feeling after yesterday?  You can get through it!");
+			break;
+		case "Wednesday":
+			System.out.println("Humpday!  Keep it going!");
+			break;
+		case "Thursday":
+			System.out.println("Throwback Thursday!  Listen to some oldies.");
+			break;	
+		case "Friday":
+			System.out.println("Almost to the weekend!  Kepp up the hard work!");
+			break;
+		case "Saturday":
+			System.out.println("Woohoo the weekend!");
+			break;
+		}
+			
+			
 		
 	}
 }
--- src/_05_Enum_Stuff/_00_Horoscope.java
+++ src/_05_Enum_Stuff/_00_Horoscope.java
@@ -8,7 +8,11 @@ public class _00_Horoscope {
 	
 	// 2. Write a method that takes in a Zodiac enum object and uses a JOPtionPane to display
 	//    a different horoscope based on the Zodiac's state.
-	
+	JOptionPane ZodiacR(DataTypes z) {
+		
+	}
 	// 3. Make a main method to test your method
-	
+	public static void main(String[] args) {
+		
+	}
 }
